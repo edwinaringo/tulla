bd0e4ec5bc55ec9a6c38a43fd56185b2e089c977
edwinaringo <tedaringo628@gmail.com>
2023-03-07 12:35:32 +0300

    Added on click to the menu button

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onClick = () =>{
+    navbar.classList.add('active');
+}
+
+// document.querySelector('#nav-close').onClick = () =>{
+//     navbar.classList.remove('active');
+// }
+
+// window.onscroll = () =>{
+//     navbar.classList.remove('active');
+
+// }
