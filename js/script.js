dbf123e517866a11cc067688e00e104e91406e2d
edwinaringo <tedaringo628@gmail.com>
2023-03-12 23:06:06 +0300

    Fixed the menu and closing the menu javascript errors

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#menu-btn').onClick = () =>{
+document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.add('active');
 }
 
-// document.querySelector('#nav-close').onClick = () =>{
-//     navbar.classList.remove('active');
-// }
+ document.querySelector('#nav-close').onclick = () =>{
+     navbar.classList.remove('active');
+ }
 
-// window.onscroll = () =>{
-//     navbar.classList.remove('active');
+ window.onscroll = () =>{
+     navbar.classList.remove('active');
 
-// }
+ }
